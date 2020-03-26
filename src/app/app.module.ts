07fa8eceac26a7b681867d8cd4f7f15a6192263c
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2020-03-26 14:07:49 +0900

    Add dashboard component
    - Add dashboard component
    - Modify routing module to show dashboard and list.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
     AppComponent,
     NodesTableComponent,
-    NavigationComponent
+    NavigationComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +39,10 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
